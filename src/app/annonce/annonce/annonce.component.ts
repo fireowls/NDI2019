8d1c9973827bc3c3668243b3e05571c537751655
TheTeKsa <aymeric03d@gmail.com>
2019-12-05 22:27:31 +0100

    annonce suite

--- src/app/annonce/annonce/annonce.component.ts
+++ src/app/annonce/annonce/annonce.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AnnonceService } from '../annonce.service';
 
 @Component({
   selector: 'app-annonce',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnonceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AnnonceService) { }
 
   ngOnInit() {
   }
