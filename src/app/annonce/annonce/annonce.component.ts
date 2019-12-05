5e9fbdfaa5a4333d050b2600b7bedc9b507c40b6
Mizaki <allan150199@outlook.fr>
2019-12-05 22:22:45 +0100

    Modif AnnonceItem, ajout des listes

--- src/app/annonce/annonce/annonce.component.ts
+++ src/app/annonce/annonce/annonce.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnnonceService } from '../annonce.service';
 
 @Component({
   selector: 'app-annonce',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnonceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AnnonceService) { }
 
   ngOnInit() {
+    
   }
 
 }
